7b6b2ea26d62458667e4467e41e245c4f3e6302e
cyberbanana777 <sashagrachev2005@gmail.com>
2026-05-23 13:33:34 +0300

    Added support for STM32 driver for lidar

--- sllidar_ros2/scripts/create_udev_rules.sh
+++ sllidar_ros2/scripts/create_udev_rules.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-echo "remap the device serial port(ttyUSBX) to  rplidar"
-echo "rplidar usb connection as /dev/rplidar , check it using the command : ls -l /dev|grep ttyUSB"
+echo "remap the device serial port(ttyUSBX / ttyACMX) to  rplidar"
+echo "rplidar usb connection as /dev/rplidar , check it using the command : ls -l /dev|grep -E 'ttyUSB|ttyACM|rplidar'"
 echo "start copy rplidar.rules to  /etc/udev/rules.d/"
-colcon_cd rplidar_ros2
 sudo cp scripts/rplidar.rules  /etc/udev/rules.d
 echo " "
 echo "Restarting udev"
